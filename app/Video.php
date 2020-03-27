df0539e9575281ef82502a214c7eefda3956cb7a
joseph.scott0605 <abC123,./>
2020-03-26 17:26:19 -0700

    video.php again

---
+++ app/Video.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Video extends Model
+{
+    //
+    
+    public function Gym()
+    {
+        return $this->belongsTo('App\Gym');
+    }
+}
