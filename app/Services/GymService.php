854da6c22deea3e348168876cada6a586c669f36
joseph.scott0605 <abC123,./>
2020-03-27 20:17:21 -0700

    Gym Service

--- app/Services/GymService.php
+++ app/Services/GymService.php
@@ -54,20 +54,9 @@ class GymService
 
 	public function approve_request($user_id, $gym_id)
 	{
-<<<<<<< HEAD
 		$gym = $this->gymRepo->find($gym_id);
 		$gym->members()->detach($user_id);
 		$gym->members()->attach($user_id, ['status' => 1]);
-=======
-<<<<<<< HEAD
-		$guser = Gym_User::where('user_id', $user_id)->where('gym_id', $gym_id)->update('status', 1);
-=======
-		$guser = Gym_User::where('user_id', $user_id)->where('gym_id', $gym_id);
-		$guser->status = 1;
-		$guser->save();
-
->>>>>>> 990eba93eb7cd6da5826027129ddb5caca227e2b
->>>>>>> 90056a4e3177cb4993a369a5cd06fe2ec84c14c8
 	}
 
 	public function denied_request($user_id, $gym_id)
