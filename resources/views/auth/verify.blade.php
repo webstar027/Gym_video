f23cdfcbb37957b101a76cf7f5562f5edc227899
joseph.scott0605 <abC123,./>
2020-03-29 21:17:14 -0700

    Verify email tempalte

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -3,6 +3,35 @@
 @section('content')
 <section class="bg-trans">
     <div class="container">
+
+        <div class="row justify-content-center">
+            
+            <div class="col-lg-6">
+                <div class="login-register">
+                    <h3 class="text-left">Verify Your Email Address</h3>
+                    <div>
+                        @if (session('resent'))
+                            <div class="alert alert-success" role="alert">
+                                {{ __('A fresh verification link has been sent to your email address.') }}
+                            </div>
+                        @endif
+
+                        {{ __('Before proceeding, please check your email for a verification link.') }}
+                        {{ __('If you did not receive the email') }},
+                        <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+                            @csrf
+                            <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        </div><!-- //.row -->
+
+    </div><!-- //.container -->
+</section>
+<!-- <section class="bg-trans">
+    <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
                 <h3>{{ __('Verify Your Email Address') }}</h3>
@@ -24,5 +53,5 @@
         </div>
     </div>
 </div>
-</section>
+</section> -->
 @endsection
